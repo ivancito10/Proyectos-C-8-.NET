300393f9b65f19d632e0a87320c49b7984eb0be2
ivancito10 <50561919+ivancito10@users.noreply.github.com>
2020-02-28 00:02:20 -0400

    Hola mundo

---
+++ Curso de net core/Curso de net core/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Curso_de_net_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 10;
+            Console.WriteLine("Hello Ivan! " + year);
+
+            Console.ReadLine();
+        }
+    }
+}
